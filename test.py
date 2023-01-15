38aa339826ab875cde4a002b076e03f430f4bbcc
Michele <micheledezen199620@gmail.com>
2023-01-15 19:01:39 +0100

    commented

--- test.py
+++ test.py
@@ -1,29 +1,38 @@
+"""
+Date: 15/01/2023
+Author: Michele De Zen
+Description: Script to extract 2 types of "error messages" from single files
+"""
+
+#Imports
 from bs4 import BeautifulSoup
 import os
 import csv
 
+#Paths
 inputPath = 'files/'
 outputPath = './out/Messages.csv'
 
 inputPathCases = 'files/Cases'
 outputPathCases = './out/Cases.csv'
 
+#Headers for the csv output
 header = ['file', 'name', 'description']
 
+#Temporary storage for extracted data
 dataMessages = []
 dataCases = []
 
-
-#Normal
+#First type of files
 for filename in os.listdir(inputPath):
     f = os.path.join(inputPath, filename)
+    #check if it's a file
     if os.path.isfile(f):
+        #log the file name to console
         print(f)
         with open(f, 'r', encoding='UTF8') as file:
 
             contents = file.read()
-
-
             soup = BeautifulSoup(contents, 'lxml')
 
             name = soup.find("td", {"class": "messageshortcut"}).text
@@ -31,6 +40,7 @@ for filename in os.listdir(inputPath):
 
             dataMessages.append([f, name, desc])
 
+#Save extracted data to csv file
 with open(outputPath, 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
@@ -41,7 +51,7 @@ with open(outputPath, 'w', encoding='UTF8', newline='') as f:
     for row in dataMessages:
         writer.writerow(row)
 
-#Cases
+#Second type of file
 for filename in os.listdir(inputPathCases):
     f = os.path.join(inputPathCases, filename)
     if os.path.isfile(f):
@@ -49,8 +59,6 @@ for filename in os.listdir(inputPathCases):
         with open(f, 'r', encoding='UTF8') as file:
 
             contents = file.read()
-
-
             soup = BeautifulSoup(contents, 'lxml')
 
             name = soup.find("a", {"class": "messageshortcut"}).text
@@ -58,6 +66,7 @@ for filename in os.listdir(inputPathCases):
 
             dataCases.append([f, name, desc])
 
+#Save extracted data to csv file
 with open(outputPathCases, 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
